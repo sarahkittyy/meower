6ea23e69135c9899c46f14c975b03642ef70a158
Sarah Ohlin <sarahohlin1@gmail.com>
2022-05-30 01:45:24 -0400

    feat: join/leave colors

--- src/messages/join.ts
+++ src/messages/join.ts
@@ -5,6 +5,7 @@ export default function join(member: GuildMember): MessageEmbed {
 		.setTitle('A new person joined! <3')
 		.setDescription(`Welcome ${member.displayName}!~<3`)
 		.setThumbnail(member.displayAvatarURL())
+		.setColor(0x84c8b1)
 		.setFooter({
 			text: `Total members: ${member.guild.memberCount}`,
 		})
