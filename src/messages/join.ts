54fc9e0b0e5d8aff77441c6ae254f9e4cf2c0077
Sarah Ohlin <sarahohlin1@gmail.com>
2022-06-01 19:42:18 -0400

    feat: prompt for sending intro in join message

--- src/messages/join.ts
+++ src/messages/join.ts
@@ -3,7 +3,9 @@ import { GuildMember, MessageEmbed } from 'discord.js';
 export default function join(member: GuildMember): MessageEmbed {
 	return new MessageEmbed() //
 		.setTitle('A new person joined! <3')
-		.setDescription(`Welcome ${member.displayName}!~<3`)
+		.setDescription(
+			`Welcome ${member.displayName}!~<3\nSend a message saying hi and why you're here and someone'll give u roles <3`
+		)
 		.setThumbnail(member.displayAvatarURL())
 		.setColor(0x84c8b1)
 		.setFooter({
